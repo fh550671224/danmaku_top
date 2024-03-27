12e07456da1614c98107ee5c51a40694578353f8
hfu11 <61071935+hfu11@users.noreply.github.com>
2024-03-27 00:32:19 -0700

    configs

---
+++ scripts/redeploy_containers.sh
@@ -0,0 +1,3 @@
+docker stop $(docker ps -aq)
+docker rm $(docker ps -aq)
+docker-compose -f ../docker-compose.yml up -d
---
+++ scripts/update_docker.sh
@@ -0,0 +1,3 @@
+docker build -t danmaku_top ../.
+docker tag danmaku_top richard1227/danmaku_top
+docker push richard1227/danmaku_top
