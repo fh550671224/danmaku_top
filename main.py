3ebd99bceff729b8f33766d46a158eb0e1caaf90
hfu11 <61071935+hfu11@users.noreply.github.com>
2024-03-28 01:52:30 -0700

    production environment move to gunicorn - bugfix

--- main.py
+++ main.py
@@ -12,6 +12,23 @@ from douyu.client_manager import ClientManager
 from server.routers import app
 from server.handlers import chatmsg_handler
 
+
+@app.before_first_request
+def initialize():
+    config = ConfigHandler()
+
+    redis = RedisClient()
+    redis.start_cronjob()
+
+    mongo = MongoClient()
+
+    room_list = mongo.get_rooms()
+    cm = ClientManager(room_list)
+    for key, value in cm.room_clients_map.items():
+        value.add_handler('chatmsg', chatmsg_handler)
+        value.start()
+
+
 if __name__ == '__main__':
     config = ConfigHandler()
 
@@ -26,5 +43,4 @@ if __name__ == '__main__':
         value.add_handler('chatmsg', chatmsg_handler)
         value.start()
 
-    if config.is_local_dev():
-        app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')
