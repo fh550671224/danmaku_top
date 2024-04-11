5ba469d7b3cb497f32c377cc474338fc3acb268c
hfu11 <61071935+hfu11@users.noreply.github.com>
2024-04-11 02:54:29 -0700

    frontend

--- scripts/update_docker.sh
+++ scripts/update_docker.sh
@@ -3,6 +3,8 @@ docker tag danmaku_top richard1227/danmaku_top
 docker push richard1227/danmaku_top
 docker pull richard1227/danmaku_top
 
+docker pull richard1227/danmaku_top_frontend
+
 docker pull redis
 docker pull mongo
 docker pull nginx
