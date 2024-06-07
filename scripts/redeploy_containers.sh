0a9bb78249ddf1ff5bf366c54a8b3579c4e8c9a9
hfu11 <61071935+hfu11@users.noreply.github.com>
2024-06-06 22:29:18 -0700

    support ssl - 2

--- scripts/redeploy_containers.sh
+++ scripts/redeploy_containers.sh
@@ -1,3 +1,6 @@
+export CRT_PATH=/home/ubuntu/crt/combined.crt
+export KEY_PATH=/home/ubuntu/crt/tls.key
+
 docker stop $(docker ps -aq)
 docker rm $(docker ps -aq)
 docker-compose -f ../docker-compose.yml up -d
